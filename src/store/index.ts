5816265de6411cd7cc756aac2d5c1405afb0e406
Liam Rosenfeld <me@liamrosenfeld.com>
2019-12-28 12:33:45 -0500

    Vuex Persistence

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
 
 import User from "@/store/modules/User.ts";
 
 Vue.use(Vuex);
 
+const vuexPersist = new VuexPersist({
+  key: "lymannhs",
+  storage: window.localStorage
+});
+
 export default new Vuex.Store({
+  plugins: [vuexPersist.plugin],
   modules: {
     user: User
   }
