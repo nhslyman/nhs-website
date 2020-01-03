2d8770eda2e7e84f0c90969b7918013bd7e3c0fe
Liam Rosenfeld <me@liamrosenfeld.com>
2020-01-02 22:31:13 -0500

    Update Typescript to 3.7.4
    optional chaining and null coalescing!

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,20 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: [
+    "plugin:vue/recommended",
+    "@vue/prettier",
+    "@vue/typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended"
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-unused-vars": "off" // broken on typescript getters
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: "@typescript-eslint/parser",
+    sourceType: "module"
   }
 };
